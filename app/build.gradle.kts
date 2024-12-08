2e6c08b4b672e2ab704fb36e66c7c66164c467a3
blueland <dbgus3577@gmail.com>
2024-12-08 23:25:14 +0900

    🚀 v1.0.1(3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.blueland.todo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
